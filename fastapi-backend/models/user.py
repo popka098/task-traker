0cfb94328b3c6126cbf069631a96154d51a969b4
Andrey Petrov <andrey.petrovich0880@gmail.com>
2025-08-23 13:45:26 +0300

    upd: user

--- fastapi-backend/models/user.py
+++ fastapi-backend/models/user.py
@@ -1,20 +1,40 @@
 from models.base import BaseModel
 from sqlmodel import Field, SQLModel
+from pydantic import EmailStr
 
 class User(BaseModel, table=True):
-    name : str = Field(min_length=3, max_length=255, index=True)
-    username : str = Field(min_length=3, max_length=255, unique=True)
+    name : str = Field(
+        min_length=3,
+        max_length=255,
+        index=True
+    )
+    username : str = Field(
+        min_length=3,
+        max_length=255,
+        unique=True
+    )
+    email : EmailStr
+    is_stuff : bool = Field(default=False)
+    is_admin : bool = Field(default=False)
 
 class UserCreate(SQLModel):
-    name : str = Field(min_length=3, max_length=255, index=True)
-    username : str = Field(min_length=3, max_length=255, unique=True)
+    name : str = Field(
+        min_length=3,
+        max_length=255,
+        index=True
+    )
+    username : str = Field(
+        min_length=3,
+        max_length=255,
+        unique=True
+    )
+    email : EmailStr
 
-class UserUpdate(SQLModel):
-    name : str = Field(min_length=3, max_length=255, index=True)
-    username : str = Field(min_length=3, max_length=255, unique=True)
+class UserUpdate(UserCreate):
+    pass
 
-class UserRead(SQLModel):
+class UserRead(UserCreate):
     id : int
-    name : str
-    username : str
+    is_stuff : bool
+    is_admin : bool
 
