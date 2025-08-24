e0a6a8fd2d46ef339f0a25263c1d8c69fb18b957
Andrey Petrov <andrey.petrovich0880@gmail.com>
2025-08-24 20:59:01 +0300

    added get subtasks by users and by globaltasks endpoints

--- fastapi-backend/routers/globaltasks_subtasks.py
+++ fastapi-backend/routers/globaltasks_subtasks.py
@@ -3,8 +3,19 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
 from database import get_session
 
+from models.global_task import GlobalTask, GlobalTaskRead
+from models.subtask import SubTask, SubTaskRead
+
 from typing import Annotated
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
 router = APIRouter(prefix="/api/globaltasks", tags=["GlobalTasks SubTasks"])
+
+@router.get("/{id}/subtasks", response_model=list[SubTaskRead])
+def get_subtasks_by_globaltaks(id: int, session: SessionDep) -> list[SubTaskRead]:
+    global_task = session.get(GlobalTask, id)
+    if not global_task:
+        raise HTTPException(404, "GlobalTask not found")
+
+    return global_task.subtasks
--- fastapi-backend/routers/users_subtasks.py
+++ fastapi-backend/routers/users_subtasks.py
@@ -3,8 +3,18 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
 from database import get_session
 
+from models.user import User
+from models.subtask import SubTask, SubTaskRead, SubTaskCreate
+
 from typing import Annotated
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
 router = APIRouter(prefix="/api/users", tags=["Users Subtasks"])
+
+@router.get("/{id}/subtasks", response_model=list[SubTaskRead])
+def read_global_tasks_by_user(id: int, session: SessionDep) -> list[SubTaskRead]:
+    user = session.get(User, id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user.subtasks
