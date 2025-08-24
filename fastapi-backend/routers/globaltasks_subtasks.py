a3dab071641e28309cc05118897bd7bf7e07e80c
Andrey Petrov <andrey.petrovich0880@gmail.com>
2025-08-24 17:26:07 +0300

    added routers

---
+++ fastapi-backend/routers/globaltasks_subtasks.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter, Depends, HTTPException
+
+from sqlmodel import Session
+from database import get_session
+
+from typing import Annotated
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+router = APIRouter(prefix="/api/globaltasks", tags=["GlobalTasks SubTasks", "Global Tasks", "SubTasks", "Tasks"])
--- fastapi-backend/routers/main_router.py
+++ fastapi-backend/routers/main_router.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
-from routers import users, global_tasks, users_globaltasks
+from routers import users, global_tasks, users_globaltasks, users_subtasks, globaltasks_subtasks, subtasks
 
 api_router = APIRouter()
 
 api_router.include_router(users.router)
 api_router.include_router(global_tasks.router)
 api_router.include_router(users_globaltasks.router)
+api_router.include_router(users_subtasks.router)
+api_router.include_router(globaltasks_subtasks.router)
+api_router.include_router(subtasks.router)
